d33238aeaad46c95a56840bf2e42e943dace7633
Killa J <joshuaalexandercross@gmail.com>
2021-04-20 18:58:02 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const { animals }= require('./data/animals');
 
 
 
-
-
-
 function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    //save animalArray as filteredResults
     let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // Save personalityTraits as a dedicated array
+        // If personalityTraits is a string, place it into a new array and save.
+        if (typeof query.personalityTraits ==='string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+
+        // Loop through each trait in the personalityTraits array:
+        personalityTraitsArray.forEach(trait => {
+            // Check the trait agaist each animal in the filteredResults array
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -19,7 +35,7 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
